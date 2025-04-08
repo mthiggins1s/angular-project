4b1583349a2880a2c804b45bd79907d989959214
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-08 12:03:51 -0500

    Refactor song component styles and HTML structure; update button styles and add click functionality

--- 01-starting-project/src/app/songs/songs.component.ts
+++ 01-starting-project/src/app/songs/songs.component.ts
@@ -11,6 +11,17 @@ const randomIndex = Math.floor(Math.random() * SONG_LIST.length);
   styleUrl: './songs.component.css'
 })
 export class SongsComponent {
-  // variable with a value of a song being drawn randomly.
+  // variable with a value of a song being drawn randomly
   selectedSong = SONG_LIST[randomIndex];
+
+
+  get imagePath() {
+    // you add 'this.' to access the class to where the getter belongs to
+    return 'assets/album-covers/' + this.selectedSong.cover
+  }
+
+  onSongClick() {
+    const randomIndex = Math.floor(Math.random() * SONG_LIST.length);
+    this.selectedSong = SONG_LIST[randomIndex];
+  }
 }
