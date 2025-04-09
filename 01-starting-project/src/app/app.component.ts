749babca813b57706949a73f7763699980499e70
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-09 10:21:48 -0500

    removed song box

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { SongsComponent } from "./songs/songs.component";
 import { SONG_LIST } from "./songs-list";
-import { SongBoxComponent } from "./song-box/song-box.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, SongsComponent, SongBoxComponent],
+  imports: [HeaderComponent, SongsComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
